f480e078371a989f0762904b38003d41ba191638
Hadiru Colley <dirudeen22@gmail.com>
2024-04-13 23:18:46 +0300

    Add content to BottomBar.tsx

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+"use client"
+
+import { sidebarLinks } from "../constants"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function BottomBar() {
+  const pathname = usePathname()
   return (
-    <div>BottomBar</div>
+    <section className="bottombar">
+      <div className="bottombar_container">
+   {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route
+        return (
+          <Link href={link.route} key={link.label} className={`bottombar_link ${isActive ? "bg-primary-500": ""}`}>
+            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+            <p className="text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+          </Link>
+        )})}
+      </div>
+      
+    </section>
   )
 }
