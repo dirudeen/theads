c670d6a53b43244c41ab9956d7fef2d6a4d6e49b
Hadiru Colley <dirudeen22@gmail.com>
2024-04-13 23:27:42 +0300

    Add content to RightSidebar.tsx

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function RightSidebar() {
   return (
-    <div>RightSidebar</div>
+    <section className='rightsidebar custom-scrollbar'>
+      <div className='flex flex-col flex-1 justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Groups</h3>
+      </div>
+    <div className='flex flex-col flex-1 justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+      </div>
+    </section>
   )
 }
