bfdb650489dbf335653d75acd641c252021a128e
Hadiru Colley <dirudeen22@gmail.com>
2024-04-16 02:10:45 +0300

    Add name, username and bio field inputs

--- src/components/form/AccountProfile.tsx
+++ src/components/form/AccountProfile.tsx
@@ -17,6 +17,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validation/user";
 import Image from "next/image";
 import { ChangeEvent } from "react";
+import { Textarea } from "../ui/textarea";
 interface Props {
   user: {
     id: string | undefined;
@@ -42,8 +43,7 @@ export default function AccountProfile({ user, btnTitle }: Props) {
 
   const imageHandler = (e: ChangeEvent, onChange: (value: string) => void) => {
     e.preventDefault();
-
-  }
+  };
 
   const onSubmit = (values: z.infer<typeof UserValidation>) => {
     // Do something with the form values.
@@ -55,11 +55,11 @@ export default function AccountProfile({ user, btnTitle }: Props) {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-10"
+        className="flex flex-col justify-start gap-10"
       >
         <FormField
           control={form.control}
-          name="username"
+          name="profile_photo"
           render={({ field }) => (
             <FormItem className="flex items-center gap-4">
               <FormLabel className="account-form_image-label">
@@ -82,22 +82,71 @@ export default function AccountProfile({ user, btnTitle }: Props) {
                   />
                 )}
               </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-grey-500">
-                <Input 
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Input
                   type="file"
-                  className="account-form_image"
-                  onChange={e => imageHandler(e, field.onChange)}
+                  className="account-form_image-input"
+                  onChange={(e) => imageHandler(e, field.onChange)}
                   accept="image/*"
+                  placeholder="Add profile photo"
                 />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Name
+              </FormLabel>
+              <FormControl>
+                <Input
+                type="text"
+                className="account-form_input no-focus"
+                {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+     <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Username
+              </FormLabel>
+              <FormControl>
+                <Input
+                type="text"
+                className="account-form_input no-focus"
+                {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+ <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+               Bio 
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                rows={10}
+                className="account-form_input no-focus"
+                {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">{btnTitle}</Button>
       </form>
     </Form>
   );
