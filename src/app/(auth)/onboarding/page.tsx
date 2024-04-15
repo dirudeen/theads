0dac0225b7d68b8052e380181d3e302f2108fc30
Hadiru Colley <dirudeen22@gmail.com>
2024-04-15 23:17:15 +0300

    Pass user info to AccountProfile component.

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,13 +1,26 @@
 import AccountProfile from "@/components/form/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
 
-export default function OnboardingPage() {
+export default async function OnboardingPage() {
+    const user = await currentUser();
+
+    // dummy userInfo
+    const userInfo = {name: "foo", bio: "", image: "", id: "", username: ""}
+    const userData = {
+        id: user?.id,
+        objectId: userInfo.id,
+        username: userInfo?.username || user?.username || "",
+        name: userInfo?.name || user?.username || "",
+        bio: userInfo?.bio || "",
+        image: userInfo.image || user?.imageUrl
+    }
     return(
         <main className="mx-auto flex flex-col px-10 py-20">
             <h1 className="head-text">Onboarding</h1>
             <p className="mt-4 text-light-2 text-base-regular">{`👋 Welcome! You're officially part of the team. Let's start this exciting journey together! 🚀`}</p>
 
             <section className="mt-9 bg-dark-2 py-10">
-                <AccountProfile />
+                <AccountProfile user={userData} btnTitle="Continue" />
             </section>
         </main>
     )
