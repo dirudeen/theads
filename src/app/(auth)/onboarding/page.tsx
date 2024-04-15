29178fe3fc42490395e534554c35f49b39caf31b
Hadiru Colley <dirudeen22@gmail.com>
2024-04-15 22:32:13 +0300

    Add content to onboarding page

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,7 +1,12 @@
 export default function OnboardingPage() {
     return(
-        <main>
-            <p className="head-text">OnboardingPage</p>
+        <main className="mx-auto flex flex-col px-10 py-20">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-4 text-light-2 text-base-regular">{`👋 Welcome! You're officially part of the team. Let's start this exciting journey together! 🚀`}</p>
+
+            <section className="mt-9 bg-dark-2 py-10">
+                <AccountProfile />
+            </section>
         </main>
     )
 }
