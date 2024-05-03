a26773a4095974868135a8ef96dbc5d69bdc2cfb
Hadiru Colley <dirudeen22@gmail.com>
2024-05-03 21:40:41 +0300

    Pass user objectId as props and format the code

--- src/app/(root)/create-thread/page.tsx
+++ src/app/(root)/create-thread/page.tsx
@@ -1,18 +1,20 @@
+import { PostThread } from "@/components/form/PostThread";
 import { fetchUser } from "@/lib/actions/user.action";
-import { currentUser } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export default async function CreateTheadPage(){
-    const user = await currentUser()
+export default async function CreateTheadPage() {
+  const user = await currentUser();
 
-    if(!user) return null;
+  if (!user) return null;
 
-    const userInfo = await fetchUser(user?.id)
+  const userInfo = await fetchUser(user?.id);
 
-    if(!userInfo.onboarded) redirect("/onboarding")
-    return (
-        <>
-            <h1 className="head-text">Create Thread</h1>
-        </>
-    )
+  if (!userInfo.onboarded) redirect("/onboarding");
+  return (
+    <>
+      <h1 className="head-text">Create Thread</h1>
+      <PostThread userId={userInfo._id.toString()} />
+    </>
+  );
 }
