44a768533cef9608d9a04109172242337b903c5b
Hadiru Colley <dirudeen22@gmail.com>
2024-05-03 22:38:27 +0300

    Add textarea and button components from shadcn ui

--- src/components/form/PostThread.tsx
+++ src/components/form/PostThread.tsx
@@ -18,7 +18,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { ThreadValidation } from "@/lib/validation/thread";
 
 
-export default function PostThread(userId: string) {
+export function PostThread({userId}: {userId: string}) {
   const pathname = usePathname()
   const router = useRouter()
 
@@ -31,6 +31,42 @@ export default function PostThread(userId: string) {
     },
   });
 
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    // await createThread({
+        
+    // })
+  };
+
   return (<>
+    <Form {...form}>
+    <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 flex flex-col justify-start gap-10"
+      >
+    <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea
+                    rows={15}
+                {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+         type="submit"
+          className="bg-primary-500 text-light-1"
+          >
+          Post Thread
+        </Button>
+        </form>
+    </Form>
   </>)
 }
